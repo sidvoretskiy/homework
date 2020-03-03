fb27768278c7206c5aa1ad33e75cfc8b2c014735
Sergey Dvoretskiy <sidvoretskiy@gmail.com>
2020-03-03 21:46:19 +0300

    Lesson 1.Weight

--- Lesson_1/weight.rb
+++ Lesson_1/weight.rb
@@ -1,2 +1,13 @@
-puts "hello"
+puts "Введите свое имя"
+user_name = gets
+puts "Введите свой рост"
+height = gets
+
+optimal = (height.to_i - 110)*1.15
+
+if optimal > 0
+	puts "Ваш оптимальный вес #{optimal}"
+else
+	puts "Ваш вес уже оптимальный"
+end
 
