9b17022343ac358c87c518233d011e14711d5421
Sergey Dvoretskiy <sidvoretskiy@gmail.com>
2020-03-10 13:23:37 +0300

    Lesson 1.Comment fix2

--- Lesson_1/right_triangle.rb
+++ Lesson_1/right_triangle.rb
@@ -9,25 +9,25 @@ max_length = [a,b,c].max
 min_length = [a,b,c].min
 
 if min_length == max_length
-	puts "Треугольник равносторонний"
+  puts "Треугольник равносторонний"
 elsif a == max_length
-	if a ** 2 == b ** 2 + c ** 2
-		puts "Треугольник прямоуголный"
-	else 
-		puts "Это обычный треугольник"
-	end
+  if a ** 2 == b ** 2 + c ** 2
+    puts "Треугольник прямоуголный"
+  else 
+    puts "Это обычный треугольник"
+  end
 elsif b == max_length
-	if b ** 2 == a ** 2 + c ** 2
-		puts "Треугольник прямоуголный"
-	else 
-		puts "Это обычный треугольник"
-	end
+  if b ** 2 == a ** 2 + c ** 2
+    puts "Треугольник прямоуголный"
+  else 
+    puts "Это обычный треугольник"
+  end
 elsif c == max_length
-	if c ** 2 == b ** 2 + a ** 2
-		puts "Треугольник прямоуголный"
-	else 
-		puts "Это обычный треугольник"
-	end				
+  if c ** 2 == b ** 2 + a ** 2
+    puts "Треугольник прямоуголный"
+  else 
+    puts "Это обычный треугольник"
+  end				
 else 
-		puts "Это обычный треугольник"
+    puts "Это обычный треугольник"
 end
--- Lesson_1/weight.rb
+++ Lesson_1/weight.rb
@@ -6,7 +6,7 @@ height = gets.to_i
 optimal = (height - 110) * 1.15
 
 if optimal > 0
-	puts "Ваш оптимальный вес #{optimal}"
+  puts "Ваш оптимальный вес #{optimal}"
 else
-	puts "Ваш вес уже оптимальный"
+  puts "Ваш вес уже оптимальный"
 end
