c5ef2f064b27ece727f6b586a11e87f7454999ac
Sergey Dvoretskiy <sidvoretskiy@gmail.com>
2020-03-15 22:02:13 +0300

    Lesson 2. Cart

---
+++ Lesson_2/6_cart_summ.rb
@@ -0,0 +1,21 @@
+cart = Hash.new
+loop do
+  puts 'Введите название товара'
+  product = gets.chomp
+  break if product == 'стоп' 
+  puts 'Введите цену товара'
+  price = gets.chomp.to_f
+  puts 'Введите колличество товара'
+  quantity = gets.chomp.to_f
+  cart[product]={price => quantity}
+end
+sum = 0
+puts cart
+cart.each do |product, price|
+  price.each do |a,b| 
+    line_sum = a * b
+    puts "#{product}, сумма за товар = #{line_sum}"
+    sum += line_sum
+  end
+end
+puts "Общая стоимость покупок: #{sum}"
